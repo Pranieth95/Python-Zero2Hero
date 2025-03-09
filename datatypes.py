e56fac0cc3e0e411f07df22666271b8ab22dc58c
Pranieth Chandrasekara <pranieth.infosec@gmail.com>
2025-03-09 11:00:41 +0530

    Data types

---
+++ datatypes.py
@@ -0,0 +1,6 @@
+print (10)  #integer
+print (10.1) #float
+print ("Strings") #string
+print ("This breaks a \n new line") # \n starts a new line
+print ("This prints a \t: tab space") # \t keeps a tab space
+print ("He said \"Good Morning\"") #\ is an escape character to print "
