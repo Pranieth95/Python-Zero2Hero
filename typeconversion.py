587c85967042d93b5980605caa01e8ad4e9b6b8e
Pranieth Chandrasekara <pranieth.infosec@gmail.com>
2025-03-11 16:46:52 +0530

    Type Conversions

---
+++ typeconversion.py
@@ -0,0 +1,13 @@
+a = "10" #now this is a string
+b = 20
+c = int (a) + b # converts a in to an integer 
+
+print (c)
+
+
+
+x = "Pranieth"
+y = 10
+z = x + str(y) # converts y into a string
+
+print (z)
--- variablesANDoperators.py
+++ variablesANDoperators.py
@@ -1,3 +1,31 @@
 name = "Pranieth"
+print (name) #print a string variable
 
-print (name)
+a = 10
+b = 4
+c = a + b
+print (c) #print a result of an operation
+
+print ( a / b) # Result of a devide by b
+print ( a % b) # Remainder of a devided by b
+print (a ** b) # Result of a to the power b
+print ( a // b) # Returns only the integer result of a devided by b
+
+age =14
+young = age < 18
+print (young) #Return a boolean value which means true or false
+
+x = True
+y = False
+z = x and y
+r = x or y
+
+print ( z ) # check the condition where both x and y are true and if yes return true
+print ( r ) # Return true if wither one of x or y is true
+
+p, q = 1, 2
+print (p, q)
+print (type(p), type(q)) #Returns the variable type
+
+
+
