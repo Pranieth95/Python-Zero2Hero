40ca40668d5fc6197bcd6c385f4720c4a0dc6538
Pranieth Chandrasekara <pranieth.infosec@gmail.com>
2025-04-16 13:13:26 +0530

    Basic for loop examples

--- forLoop.py
+++ forLoop.py
@@ -1,7 +1,13 @@
-# Program to print the square of all non-negative integers less than a given number n
+# Program to demonstrate basic for loops in Python
 
 if __name__ == '__main__':
     n = int(input("Enter a number: "))  # Read an integer from standard input
 
     for i in range(n):  # Loop through all integers from 0 to n-1
         print(i * i)    # Print the square of each number
+
+
+    
+    for i in range (1, n + 1): # Loop through all integers from 1 to n
+        print(i, end="") #Print in a single line instead of a new line for each result using end=""
+        
